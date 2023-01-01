904ce3f35368fa99205cddbc7f0fb7830003ad8f
Arian <ariannedjabat@gmail.com>
2023-01-01 16:05:01 -0500

    Update db scripts and docker compose

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS beaches (
+DROP TABLE IF EXISTS beaches;
+CREATE TABLE beaches (
   id SERIAL,
   name varchar(250) NOT NULL,
   rating int NOT NULL,
   description varchar(250) NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO beaches(name,rating,description) VALUES
+('Coco beach',4,'Test description.'),
+('Long beach',2,'Test description.'),
+('Wasaga beach',1,'Test description.');
